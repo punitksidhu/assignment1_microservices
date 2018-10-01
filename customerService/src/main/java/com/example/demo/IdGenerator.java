344d0beb0e5c704127c6dfce4a9ec9bdd33420d6
lisa <716173@PC352463.cts.com>
2018-10-01 13:52:32 +0530

    conmfig

---
+++ customerService/src/main/java/com/example/demo/IdGenerator.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IdGenerator  implements IdentifierGenerator {
+
+	@Autowired
+	private Configuration configuration;
+	//String prefix = "DEV";
+
+	public Serializable generate(SharedSessionContractImplementor  session, Object object) throws HibernateException {
+		// TODO Auto-generated method stub
+		
+		String prefix=configuration.getEnv();
+		 Connection connection = session.connection();
+
+		    try {
+		        Statement statement=connection.createStatement();
+
+		        ResultSet rs=statement.executeQuery("select count(CUSTOMER_ID) as Id from customers");
+
+		        if(rs.next())
+		        {
+		            int id=rs.getInt(1)+101;
+		            String generatedId = prefix + new Integer(id).toString();
+		            return generatedId;
+		        }
+		    } catch (SQLException e) {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+		    }
+
+		    return null;
+	}
+	
+
+}
